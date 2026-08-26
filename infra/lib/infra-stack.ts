a87c90dfdc0ec86c38332ed4df93306a3f40375e
kushyme <159010501+kushyme@users.noreply.github.com>
2026-08-26 11:02:18 +0200

    Created the initial draft for the aws infrastructure

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,16 +1,28 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Cluster, KubernetesVersion } from "aws-cdk-lib/aws-eks-v2";
+import { Repository } from "aws-cdk-lib/aws-ecr";
+import { Vpc, IpAddresses } from "aws-cdk-lib/aws-ec2"
+
+interface InfraStackProps extends cdk.StackProps {
+  prefix: string | false
+}
 
 export class InfraStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: InfraStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new Vpc(this, "vpc", {
+      ipAddresses: IpAddresses.cidr("10.1.1.0/30"),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const ecrRepository = new Repository(this, "ecrRepository", {
+      imageScanOnPush: true,
+    });
+    
+    const cluster = new Cluster(this, "eksCluster", {
+      version: KubernetesVersion.V1_36,
+    });
+    
   }
 }
